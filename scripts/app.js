f54aae6de98126182a96f2c44e2e93c41a4c4628
Shubham Saha <imshubhamsaha@gmail.com>
2024-03-15 13:45:39 +0530

    grid-mini-project/wip/layout page/update in navbar- added home refrence

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const navBtn = document.getElementById('nav-btn');
-const navbar = document.getElementById('navbar');
-const navClose = document.getElementById('nav-close');
+const navBtn = document.querySelector('.nav-btn');
+const navbar = document.querySelector('.navbar');
+const navClose = document.querySelector('.nav-close');
 
 navBtn.addEventListener('click', () => navbar.classList.add('show-nav'));
 navClose.addEventListener('click', () => navbar.classList.remove('show-nav'));
