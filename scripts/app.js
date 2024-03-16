46ba75301c793bc91d2e878625512fdbb46e98b5
Shubham Saha <imshubhamsaha@gmail.com>
2024-03-17 01:51:45 +0530

    Grid-mini-project/feat- integrated navbar and grid styles

--- scripts/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const navBtn = document.querySelector('.nav-btn');
-const navbar = document.querySelector('.navbar');
-const navClose = document.querySelector('.nav-close');
-
-navBtn.addEventListener('click', () => navbar.classList.add('show-nav'));
-navClose.addEventListener('click', () => navbar.classList.remove('show-nav'));
